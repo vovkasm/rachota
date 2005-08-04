1fa9cf5f3961e2d9186302c0b568944dc0a52029
cesilko <unknown>
2005-08-04 12:13:32 +0000

    Initial import.

---
+++ src/org/cesilko/rachota/core/ChangeHandler.java
@@ -0,0 +1,85 @@
+/*
+ * ChangeHandler.java
+ *
+ * Created on July 23, 2004, 8:56 PM
+ */
+
+package org.cesilko.rachota.core;
+
+import java.util.Hashtable;
+import java.util.Iterator;
+import java.util.Vector;
+
+/** Singleton responsible for distribution of all change events fired in the system.
+ *
+ * @author  Jiri Kovalsky
+ */
+public class ChangeHandler {
+    
+    /**
+     * The only instance of ChangeHandler object in the system.
+     */
+    private static ChangeHandler ChangeHandler;
+    
+    /**
+     * Table of all change event listeners in the system grouped by the changing objects.
+     */
+    private Hashtable changeEventListeners;
+    
+    /**
+     * Creates a new instance of ChangeHandler class.
+     */
+    private ChangeHandler() {
+        changeEventListeners = new Hashtable();
+    }
+    
+    /** Returns default instance of ChangeHandler class.
+     * @return Default instance of ChangeHandler class.
+     */
+    public static ChangeHandler getDefault() {
+        if (ChangeHandler == null)
+            ChangeHandler = new ChangeHandler();
+        return ChangeHandler;
+    }
+    
+    /** Registers new change event listener by given object of interest.
+     * @param changeEventListener New change event listener for registration.
+     * @param object Object that is listener interested in.
+     */
+    public void addChangeEventListener(ChangeListener changeEventListener, Object object) {
+        Vector allObjectChangeEventListeners = (Vector) changeEventListeners.get(object);
+        if (allObjectChangeEventListeners == null) {
+            allObjectChangeEventListeners = new Vector();
+            allObjectChangeEventListeners.add(changeEventListener);
+            changeEventListeners.put(object, allObjectChangeEventListeners);
+        } else
+            allObjectChangeEventListeners.add(changeEventListener);
+    }
+    
+    /** Removes change event listener with given object of interest from list of registered listeners.
+     * @param changeEventListener Registered change event listener.
+     * @param object Object that is listener interested in.
+     */
+    public void removeChangeEventListener(ChangeListener changeEventListener, Object object) {
+        Vector allObjectChangeEventListeners = (Vector) changeEventListeners.get(object);
+        if (allObjectChangeEventListeners != null)
+            allObjectChangeEventListeners.remove(changeEventListener);
+        if (allObjectChangeEventListeners.size() == 0)
+            changeEventListeners.remove(object);
+    }
+    
+    /** Informs all of registered listeners with interest in given object about the event.
+     * @param object Object that fired the event.
+     * @param changeType Type of change that happened on object.
+     */
+    public void fireEvent(Object object, int changeType) {
+        Vector allObjectChangeEventListeners = (Vector) changeEventListeners.get(object);
+        if (allObjectChangeEventListeners != null) {
+            Iterator iterator = allObjectChangeEventListeners.iterator();
+            while (iterator.hasNext()) {
+                ChangeListener changeEventListener = (ChangeListener) iterator.next();
+                changeEventListener.eventFired(object, changeType);
+            }
+        }
+    }
+}
---
+++ src/org/cesilko/rachota/core/Clock.java
@@ -0,0 +1,117 @@
+/*
+ * Clock.java
+ *
+ * Created on 10. duben 2003, 21:15
+ */
+
+package org.cesilko.rachota.core;
+
+import java.util.Iterator;
+import java.util.Vector;
+import org.cesilko.rachota.core.ClockListener;
+
+/**
+ * System clock ticking every second by default.
+ * @author Jiri Kovalsky
+ */
+public class Clock extends Thread {
+    
+    /**
+     * Clock period i.e. time gap between two ticks.
+     */
+    private long tick;
+    
+    /**
+     * Set of clock listeners that want to be notified at each tick.
+     */
+    
+    private Vector listeners;
+    
+    /**
+     * True if clock is ticking.
+     */
+    private boolean ticking;
+    
+    /**
+     * The only instance of clock object in the system.
+     */
+    private static Clock clock;
+    
+    /** Returns default instance of Clock object. Tick period is initially set to 1000 ms.
+     * @return The only instance of clock in the system.
+     */
+    public static Clock getDefault() {
+        if (clock == null) clock = new Clock(1000);
+        return clock;
+    }
+    
+    /** Clock constructor.
+     * @param tick Required clock tick in milliseconds.
+     */
+    private Clock(long tick) {
+        listeners = new Vector();
+        setTick(tick);
+        resumeClock();
+    }
+    
+    /** Sets period of one clock tick.
+     * @param tick Clock period i.e. time between two notifications to listeners.
+     */
+    public void setTick(long tick) {
+        this.tick = tick;
+    }
+    
+    /** Adds new clock listener.
+     * @param listener Object who whats to be notified after each tick.
+     */
+    public void addListener(ClockListener listener) {
+        if (!listeners.contains(listener)) listeners.add(listener);
+    }
+
+    /** Removes already registered clock listener.
+     * @param listener Object who no longer whats to be notified after each tick.
+     */
+    public void removeListener(ClockListener listener) {
+        if (listeners.contains(listener)) listeners.remove(listener);
+    }
+    
+    /** Main clock loop. Nothing happens for given tick period and then all
+     * listeners are notified about that fact.
+     */
+    public void run() {
+        while (true) {
+            try { sleep(tick); }
+            catch (InterruptedException e) {
+                System.out.println("Warning: System clock terminated by user.");
+                e.printStackTrace();
+            }
+            if (ticking) {
+                Iterator iterator = listeners.iterator();
+                while (iterator.hasNext()) {
+                    ClockListener listener = (ClockListener) iterator.next();
+                    listener.tick();
+                }
+            }
+        }
+    }
+    
+    /** Temporarily stops clock.
+     */
+    public void suspendClock() {
+        ticking = false;
+    }
+
+    /** Starts clock again.
+     */
+    public void resumeClock() {
+        ticking = true;
+    }
+    
+    /**
+     * Returns whether clock is ticking or not.
+     * @return True if clock is ticking or false otherwise.
+     */
+    public boolean isTicking() {
+        return ticking;
+    }
+}
