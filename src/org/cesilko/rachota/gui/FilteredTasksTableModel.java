3b153c9aa4673ad007a14e9eeccba80b65b4f8bc
cesilko <unknown>
2005-10-16 19:25:54 +0000

    Sorting algorithm optimized to improve performance radically. All table values are cached first and the process itself is run in a separate thread. This is a fix of issue 1314894.

--- src/org/cesilko/rachota/gui/FilteredTasksTableModel.java
+++ src/org/cesilko/rachota/gui/FilteredTasksTableModel.java
@@ -5,9 +5,10 @@
  */
 
 package org.cesilko.rachota.gui;
-
 import java.util.Iterator;
 import java.util.Vector;
+import javax.swing.ProgressMonitor;
+import javax.swing.SwingUtilities;
 import javax.swing.table.AbstractTableModel;
 import org.cesilko.rachota.core.Task;
 import org.cesilko.rachota.core.Translator;
@@ -30,25 +31,25 @@ public class FilteredTasksTableModel extends AbstractTableModel {
     
     /** Creates a new instance of FilteredTasksTableModel */
     public FilteredTasksTableModel() {
-        setGroupSameTasks(true);
-        setTasks(new Vector());
+	setGroupSameTasks(true);
+	setTasks(new Vector());
     }
     
     /** Sets whether to group tasks with same description or not.
      * @param groupSameTasks If true, tasks with same description will occupy one row.
      */
     public void setGroupSameTasks(boolean groupSameTasks) {
-        this.groupSameTasks = groupSameTasks;
-        fireTableDataChanged();
+	this.groupSameTasks = groupSameTasks;
+	fireTableDataChanged();
     }
     
     /** Sets filtered tasks with new vector and refreshes the table.
      * @param tasks Vector of filtered tasks to be displayed in the table.
      */
     public void setTasks(Vector tasks) {
-        this.tasks = tasks;
-        sortTable(sortedColumn, false);
-        fireTableDataChanged();
+	this.tasks = tasks;
+	sortTable(sortedColumn, false);
+	fireTableDataChanged();
     }
     
     /** Returns value of cell located in given row and column.
@@ -57,23 +58,23 @@ public class FilteredTasksTableModel extends AbstractTableModel {
      * @return Value of cell at specified location.
      */
     public Object getValueAt(int row, int column) {
-        switch(column) {
-            case DESCRIPTION:
-                if (!groupSameTasks) {
-                    Task task = (Task) tasks.get(row);
-                    return task.getDescription();
-                } else return getRow(row).get(DESCRIPTION);
-            case DURATION_TIME:
-                if (!groupSameTasks) {
-                    Task task = (Task) tasks.get(row);
-                    return Tools.getTime(task.getDuration());
-                } else return getRow(row).get(DURATION_TIME);
-            case DURATION_DAYS:
-                if (!groupSameTasks) return "1";
-                else return getRow(row).get(DURATION_DAYS);
-            default:
-                return "N/A";
-        }
+	switch(column) {
+	    case DESCRIPTION:
+		if (!groupSameTasks) {
+		    Task task = (Task) tasks.get(row);
+		    return task.getDescription();
+		} else return getRow(row).get(DESCRIPTION);
+	    case DURATION_TIME:
+		if (!groupSameTasks) {
+		    Task task = (Task) tasks.get(row);
+		    return Tools.getTime(task.getDuration());
+		} else return getRow(row).get(DURATION_TIME);
+	    case DURATION_DAYS:
+		if (!groupSameTasks) return "1";
+		else return getRow(row).get(DURATION_DAYS);
+	    default:
+		return "N/A";
+	}
     }
     
     /** Returns number of rows in the table i.e. filtered tasks. The
@@ -82,15 +83,15 @@ public class FilteredTasksTableModel extends AbstractTableModel {
      * either unique or same depending on whether they should be grouped.
      */
     public int getRowCount() {
-        if (!groupSameTasks) return tasks.size();
-        return getUniqueTaskDescriptions().size();
+	if (!groupSameTasks) return tasks.size();
+	return getUniqueTaskDescriptions().size();
     }
     
     /** Returns number of columns in the table i.e. 3.
      * @return Always 3 since table has three columns.
      */
     public int getColumnCount() {
-        return 3;
+	return 3;
     }
     
     /** Returns name of column by given column number.
@@ -98,46 +99,46 @@ public class FilteredTasksTableModel extends AbstractTableModel {
      * @return Name of column by given column number.
      */
     public String getColumnName(int column) {
-        String suffix = sortingOrder == ASCENDING ? " [+]" : " [-]";
-        switch (column) {
-            case DESCRIPTION:
-                suffix = sortedColumn == DESCRIPTION ? suffix : "";
-                return Translator.getTranslation("TASKS.DESCRIPTION") + suffix;
-            case DURATION_TIME:
-                suffix = sortedColumn == DURATION_TIME ? suffix : "";
-                return Translator.getTranslation("TASKS.DURATION_TIME") + suffix;
-            case DURATION_DAYS:
-                suffix = sortedColumn == DURATION_DAYS ? suffix : "";
-                return Translator.getTranslation("TASKS.DURATION_DAYS") + suffix;
-        } return null;
+	String suffix = sortingOrder == ASCENDING ? " [+]" : " [-]";
+	switch (column) {
+	    case DESCRIPTION:
+		suffix = sortedColumn == DESCRIPTION ? suffix : "";
+		return Translator.getTranslation("TASKS.DESCRIPTION") + suffix;
+	    case DURATION_TIME:
+		suffix = sortedColumn == DURATION_TIME ? suffix : "";
+		return Translator.getTranslation("TASKS.DURATION_TIME") + suffix;
+	    case DURATION_DAYS:
+		suffix = sortedColumn == DURATION_DAYS ? suffix : "";
+		return Translator.getTranslation("TASKS.DURATION_DAYS") + suffix;
+	} return null;
     }
     
     /** Returns vector of all unique task descriptions.
      * @return Vector of all unique task descriptions.
      */
     private Vector getUniqueTaskDescriptions() {
-        Vector uniqueTasks = new Vector();
-        Iterator iterator = tasks.iterator();
-        while (iterator.hasNext()) {
-            Task task = (Task) iterator.next();
-            if (!uniqueTasks.contains(task.getDescription()))
-                uniqueTasks.add(task.getDescription());
-        }
-        return uniqueTasks;
+	Vector uniqueTasks = new Vector();
+	Iterator iterator = tasks.iterator();
+	while (iterator.hasNext()) {
+	    Task task = (Task) iterator.next();
+	    if (!uniqueTasks.contains(task.getDescription()))
+		uniqueTasks.add(task.getDescription());
+	}
+	return uniqueTasks;
     }
     
     /** Returns total time spent on all displayed tasks.
      * @return Total time spent on all displayed tasks in miliseconds.
      */
-    public long getTotalTime() {
-        Vector uniqueTasks = new Vector();
-        Iterator iterator = tasks.iterator();
-        long duration = 0;
-        while (iterator.hasNext()) {
-            Task task = (Task) iterator.next();
-            duration = duration + task.getDuration();
-        }
-        return duration;
+    public double getTotalTime() {
+	Vector uniqueTasks = new Vector();
+	Iterator iterator = tasks.iterator();
+	double duration = 0;
+	while (iterator.hasNext()) {
+	    Task task = (Task) iterator.next();
+	    duration = duration + task.getDuration();
+	}
+	return duration;
     }
     
     /** Returns vector of three objects that should be displayed at given row
@@ -147,22 +148,22 @@ public class FilteredTasksTableModel extends AbstractTableModel {
      * tasks with the description and number of days the tasks were planned for.
      */
     private Vector getRow(int row) {
-        String description = (String) getUniqueTaskDescriptions().get(row);
-        Iterator iterator = tasks.iterator();
-        int days = 0;
-        long duration = 0;
-        while (iterator.hasNext()) {
-            Task task = (Task) iterator.next();
-            if (task.getDescription().equals(description)) {
-                duration = duration + task.getDuration();
-                days = days + 1;
-            }
-        }
-        Vector rowData = new Vector();
-        rowData.add(description);
-        rowData.add(Tools.getTime(duration));
-        rowData.add("" + days);
-        return rowData;
+	String description = (String) getUniqueTaskDescriptions().get(row);
+	Iterator iterator = tasks.iterator();
+	int days = 0;
+	long duration = 0;
+	while (iterator.hasNext()) {
+	    Task task = (Task) iterator.next();
+	    if (task.getDescription().equals(description)) {
+		duration = duration + task.getDuration();
+		days = days + 1;
+	    }
+	}
+	Vector rowData = new Vector();
+	rowData.add(description);
+	rowData.add(Tools.getTime(duration));
+	rowData.add("" + days);
+	return rowData;
     }
     
     /** Currently selected sorting column. */
@@ -178,56 +179,81 @@ public class FilteredTasksTableModel extends AbstractTableModel {
      * @param column Column that will be used for sorting.
      * @param changeOrder Should the sorting order be changed ?
      */
-    public void sortTable(int column, boolean changeOrder) {
-        int count = getRowCount();
-        Vector sortedRows = new Vector();
-        for (int i=0; i<count; i++) {
-            Object maxValue = null;
-            int maxRow = -1;
-            for (int row=0; row<count; row++) {
-                if (sortedRows.contains(new Integer(row))) continue;
-                switch(column) {
-                    case DESCRIPTION:
-                        String description = (String) getValueAt(row, column);
-                        if ((maxValue == null) || (description.compareTo((String) maxValue) > 0)) {
-                            maxValue = description;
-                            maxRow = row;
-                        } break;
-                    case DURATION_TIME:
-                        long time = Tools.getTime((String) getValueAt(row, column));
-                        if ((maxValue == null) || (time > ((Long) maxValue).longValue())) {
-                            maxValue = new Long(time);
-                            maxRow = row;
-                        } break;
-                    case DURATION_DAYS:
-                        int days = (Integer.parseInt((String) getValueAt(row, column)));
-                        if ((maxValue == null) || (days > ((Integer) maxValue).intValue())) {
-                            maxValue = new Integer(days);
-                            maxRow = row;
-                        } break;
-                }
-            }
-            sortedRows.add(new Integer(maxRow));
-        }
-        Vector newSortedTasks = new Vector();
-        Vector oldTasks = (Vector) tasks.clone();
-        if (changeOrder) sortingOrder = sortedColumn == column ? !sortingOrder : ASCENDING;
-        sortedColumn = column;
-        for (int i=0; i<count; i++) {
-            Integer row = (Integer) sortedRows.get(i);
-            String description = (String) getValueAt(row.intValue(), DESCRIPTION);
-            Iterator iterator = oldTasks.iterator();
-            while (iterator.hasNext()) {
-                Task task = (Task) iterator.next();
-                if (task.getDescription().equals(description)) {
-                    if (sortingOrder == DESCENDING) newSortedTasks.add(task);
-                    else newSortedTasks.insertElementAt(task, 0);
-                    oldTasks.remove(task);
-                    iterator = oldTasks.iterator();
-                }
-            }
-        }
-        tasks = newSortedTasks;
-        fireTableDataChanged();
+    public void sortTable(final int column, final boolean changeOrder) {
+	final ProgressMonitor pm = new ProgressMonitor(null, Translator.getTranslation("MESSAGE.PROGRESS_HISTORY"), null, 0, getRowCount() - 1);
+	Thread sortingThread = new Thread() {
+	    public void run() {
+		int count = getRowCount();
+		Vector descriptions = new Vector();
+		Vector duration_times = new Vector();
+		Vector duration_days = new Vector();
+		for (int row=0; row<count; row++) {
+		    Vector rowData = getRow(row);
+		    descriptions.add(rowData.get(DESCRIPTION));
+		    duration_times.add(rowData.get(DURATION_TIME));
+		    duration_days.add(rowData.get(DURATION_DAYS));
+		    
+		    final int progress = row;
+		    if ((row % 10 == 0) || (row == count-1)) {
+			SwingUtilities.invokeLater(new Runnable() {
+			    public void run() {
+				pm.setProgress(progress);
+			    }
+			});
+		    }
+		    
+		}
+		Vector sortedRows = new Vector();
+		for (int i=0; i<count; i++) {
+		    Object maxValue = null;
+		    int maxRow = -1;
+		    for (int row=0; row<count; row++) {
+			if (sortedRows.contains(new Integer(row))) continue;
+			switch(column) {
+			    case DESCRIPTION:
+				String description = (String) descriptions.get(row);
+				if ((maxValue == null) || (description.compareTo((String) maxValue) > 0)) {
+				    maxValue = description;
+				    maxRow = row;
+				} break;
+			    case DURATION_TIME:
+				long time = Tools.getTime((String) duration_times.get(row));
+				if ((maxValue == null) || (time > ((Long) maxValue).longValue())) {
+				    maxValue = new Long(time);
+				    maxRow = row;
+				} break;
+			    case DURATION_DAYS:
+				int days = (Integer.parseInt((String) duration_days.get(row)));
+				if ((maxValue == null) || (days > ((Integer) maxValue).intValue())) {
+				    maxValue = new Integer(days);
+				    maxRow = row;
+				} break;
+			}
+		    }
+		    sortedRows.add(new Integer(maxRow));
+		}
+		Vector newSortedTasks = new Vector();
+		Vector oldTasks = (Vector) tasks.clone();
+		if (changeOrder) sortingOrder = sortedColumn == column ? !sortingOrder : ASCENDING;
+		sortedColumn = column;
+		for (int i=0; i<count; i++) {
+		    Integer row = (Integer) sortedRows.get(i);
+		    String description = (String) getValueAt(row.intValue(), DESCRIPTION);
+		    Iterator iterator = oldTasks.iterator();
+		    while (iterator.hasNext()) {
+			Task task = (Task) iterator.next();
+			if (task.getDescription().equals(description)) {
+			    if (sortingOrder == DESCENDING) newSortedTasks.add(task);
+			    else newSortedTasks.insertElementAt(task, 0);
+			    oldTasks.remove(task);
+			    iterator = oldTasks.iterator();
+			}
+		    }
+		}
+		tasks = newSortedTasks;
+		fireTableDataChanged();
+	    }
+	};
+	sortingThread.start();
     }
 }
