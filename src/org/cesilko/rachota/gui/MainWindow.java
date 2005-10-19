b532c6c7286a5168a9da4e0e199cfb275409c9b5
cesilko <unknown>
2005-10-19 05:18:08 +0000

    Upgrading build number.

--- src/org/cesilko/rachota/gui/MainWindow.java
+++ src/org/cesilko/rachota/gui/MainWindow.java
@@ -276,7 +276,7 @@ public class MainWindow extends javax.swing.JFrame implements ChangeListener {
     /** Name and version of application. */
     protected static final String title = "Rachota 2.0 RC";
     /** Build number. */
-    protected static final String build = "#050918";
+    protected static final String build = "#051018";
     /** Index of day view tab. */
     private static final int TAB_DAY_VIEW = 0;
     /** Index of history view tab. */
