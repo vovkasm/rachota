1952cf74f274c7ac461b1be7c5e990f6a1e98ea0
cesilko <unknown>
2005-10-27 22:12:19 +0000

    Just a version increase.

--- src/org/cesilko/rachota/gui/MainWindow.java
+++ src/org/cesilko/rachota/gui/MainWindow.java
@@ -298,7 +298,7 @@ public class MainWindow extends javax.swing.JFrame implements PropertyChangeList
     /** Name and version of application. */
     protected static final String title = "Rachota 2.0 RC";
     /** Build number. */
-    protected static final String build = "#051022";
+    protected static final String build = "#051028";
     /** Index of day view tab. */
     private static final int TAB_DAY_VIEW = 0;
     /** Index of history view tab. */
