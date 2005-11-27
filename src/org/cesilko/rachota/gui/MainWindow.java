955880885cfbf6864fa3318c91bccb1b3cf1035d
cesilko <unknown>
2005-11-27 20:08:48 +0000

    Just a build number increase.

--- src/org/cesilko/rachota/gui/MainWindow.java
+++ src/org/cesilko/rachota/gui/MainWindow.java
@@ -296,7 +296,7 @@ public class MainWindow extends javax.swing.JFrame implements PropertyChangeList
     /** Name and version of application. */
     protected static final String title = "Rachota 2.0 RC";
     /** Build number. */
-    protected static final String build = "#051124";
+    protected static final String build = "#051127";
     /** Index of day view tab. */
     private static final int TAB_DAY_VIEW = 0;
     /** Index of history view tab. */
