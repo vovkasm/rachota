b01465477b2f5e582a4a7fd25a76a3313d003c0c
cesilko <unknown>
2008-01-30 22:31:38 +0000

    Updated year of copyrights.

--- src/org/cesilko/rachota/gui/AboutDialog.java
+++ src/org/cesilko/rachota/gui/AboutDialog.java
@@ -117,7 +117,7 @@ public class AboutDialog extends javax.swing.JDialog {
         pnInfo.add(lbEmail, gridBagConstraints);
 
         lbCopyright.setFont(getFont());
-        lbCopyright.setText("2007 ©");
+        lbCopyright.setText("2008 ©");
         gridBagConstraints = new java.awt.GridBagConstraints();
         gridBagConstraints.gridy = 4;
         gridBagConstraints.anchor = java.awt.GridBagConstraints.WEST;
