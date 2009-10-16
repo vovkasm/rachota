18d2e80eb39bc0d579626846ec8e727c2792ee69
cesilko <unknown>
2009-10-16 20:45:46 +0000

    Removed testing main method.

--- src/org/cesilko/rachota/gui/InactivityReminderDialog.java
+++ src/org/cesilko/rachota/gui/InactivityReminderDialog.java
@@ -360,28 +360,6 @@ public class InactivityReminderDialog extends javax.swing.JDialog {
         checkAccess();
     }//GEN-LAST:event_rbNothingActionPerformed
 
-    /**
-    * @param args the command line arguments
-    */
-    public static void main(String args[]) {
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                try {
-                    Plan.loadPlan();
-                } catch (Exception ex) {
-                    ex.printStackTrace();
-                }
-                InactivityReminderDialog dialog = new InactivityReminderDialog(new javax.swing.JFrame(), (Task) Plan.getDefault().getDay(new Date()).getTasks().firstElement());
-                dialog.addWindowListener(new java.awt.event.WindowAdapter() {
-                    public void windowClosing(java.awt.event.WindowEvent e) {
-                        System.exit(0);
-                    }
-                });
-                dialog.setVisible(true);
-            }
-        });
-    }
-
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btOK;
     private javax.swing.JCheckBox chbIgnore;
